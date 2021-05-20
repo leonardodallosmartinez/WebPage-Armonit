f75a9aac7ce2d403d61f72139f6cb3c2ff79957f
Leonardo Dallos <leo3.14@hotmail.com>
2021-05-20 11:43:32 -0500

    Primer commit - inicio del proyecto

---
+++ pruebaPHP.php
@@ -0,0 +1,5 @@
+<?php
+$verduras= array("peras", "mangos", "sandias");
+echo "esta es una variable: $verduras[1]";
+
+?>
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log('hola');
+
+document.getElementById('titulo_1').innerHTML='Bueno dias Visitante';
+document.getElementById('titulo_1').style.fontSize='4em';
+document.write('<br>','Hola Señores, saludo desde document.write');
